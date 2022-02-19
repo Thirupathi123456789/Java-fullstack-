be9e631b73832e4f594f203028abedaa763a90df
Thirupathi123456789 <98969164+Thirupathi123456789@users.noreply.github.com>
2022-02-19 11:38:11 +0530

    Add files via upload

---
+++ session 3/session 4/session 5/Address.java
@@ -0,0 +1,17 @@
+package com.simplilearn.inheritance;
+
+public class Address {
+
+	String city;
+	String state;
+	int pincode;
+	
+	public Address(String city, String state, int pincode) {
+		
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+	}
+	
+	
+}
---
+++ session 3/session 4/session 5/TestAggregation.java
@@ -0,0 +1,14 @@
+package com.simplilearn.inheritance;
+
+public class TestAggregation {
+
+
+	public static void main(String[] args) {
+		
+		Address address1=new Address("Mumbai", "Maharasthra", 307282);
+		
+		Student st1=new Student(1, "Sonam", "sonam@gmail.com", address1);
+		
+		st1.display();
+	}
+}
