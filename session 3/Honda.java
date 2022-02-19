20b53adeeabcc38c6599307d3b9c2fa084d14f2a
Thirupathi123456789 <98969164+Thirupathi123456789@users.noreply.github.com>
2022-02-19 11:27:29 +0530

    Add files via upload

---
+++ session 3/Honda.java
@@ -0,0 +1,16 @@
+package com.simplilearn.annoyinner;
+
+public class Honda implements Car{
+
+	@Override
+	public void start() {
+		System.out.println("Car started");
+		
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Car Stopped");
+		
+	}
+}
